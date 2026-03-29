4e812f1eb2e53486066ab88d9e74f6a66776bc5d
kzorastudio <kzora.studio@gmail.com>
2026-03-30 01:41:37 +0300

    Update from Multi-Git Manager

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,9 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
-export default withAuth(
-  function middleware(req) {
-    return NextResponse.next()
-  },
-  {
-    pages: {
-      signIn: '/admin/login',
-    },
-    callbacks: {
-      authorized: ({ token, req }) => {
-        if (req.nextUrl.pathname === '/admin/login') return true
-        return !!token
-      },
-    },
-  }
-)
+export default function middleware(req: any) {
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: ['/admin/:path*', '/admin'],
