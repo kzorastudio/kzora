628451b6f49437e1cf5e3041dd00430a572fdf70
kzorastudio <kzora.studio@gmail.com>
2026-03-31 02:26:31 +0300

    Update from Multi-Git Manager

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,5 @@
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import { Suspense } from 'react'
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
